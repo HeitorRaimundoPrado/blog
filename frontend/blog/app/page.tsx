40f09a5ebf2194ec3716378724c9ca845b16f393
HeitorRaimundoPrado <heitorrdpp@gmail.com>
2023-12-16 19:06:02 -0300

    feat: finish landing page styles

--- frontend/blog/app/page.tsx
+++ frontend/blog/app/page.tsx
@@ -19,19 +19,70 @@ export default function Home() {
             <p className='mt-6'>Sharing my coding experiences</p>
           </div>
         </div>
-        <Image src="/landing_page_top.png" width={300} height={300} alt="background image" style={{ position: "relative", bottom: "18rem", left: "50rem" }} />
-        <div className='mt-28 flex flex-row'>
+        <Image src="/landing_page_top.png" width={300} height={300} alt="background image" style={{ position: "relative", bottom: "19rem", left: "50rem" }} />
+        <div className='mt-10 flex flex-row mb-20'>
           <div className='bg-green-400 flex w-1/4 h-40 flex flex-col pt-8 rounded-md info-square-1'>
             <h2 className='font-bold text-center text-xl'>Remember to always KISS</h2>
             <p className='ml-28'>Keep it simple stupid</p>
           </div>
-          <div className="ml-12">
-            <h2 className="text-lg  uppercase relative bottom-8 text-gray-400">Feature</h2>
-            <h1 className="font-bold text-lg mb-4">Simplicity and Efficiency</h1>
-            <p>I specialize in Computer Science, programming specially web-development and competitive programming,
+          <div className="ml-12 info-square-2 relative bottom-6">
+            <h2 className="text-lg  uppercase relative bottom-4 text-gray-400">Feature</h2>
+            <h1 className="font-bold text-lg  relative">Simplicity and Efficiency</h1>
+            <p className="text-justify mb-6">I specialize in Computer Science, programming specially web-development and competitive programming,
               and I also like to share my knowledge with others</p>
-
-            <a href="#" className="text-sm text-gray-400">Explore more</a>
+            <a href="#" className="text-sm text-gray-400 ml-6">Explore more</a>
+          </div>
+        </div>
+        <div className="mt-48 flex flex-row">
+          <div className="info-square-4">
+            <h2 className="text-lg uppercase relative bottom-4 text-gray-400">Feature</h2>
+            <h1 className="font-bold text-lg mb-4 ">Simplicity is Key</h1>
+            <p className="text-justify mb-8">A minimalist approach to coding enables me to deliver high quality content without unecessary complexity or clutter.
+              I focus on what's important, and my readers appreciate the effiency and effectiveness of my articles.
+            </p>
+            <a href="#" className="text-sm text-gray-400 ml-4">Read More</a>
+          </div>
+          <div className="info-square-3 rounded-md flex ml-16">
+            <Image src='/landing-page-image2.png' alt="background image" width={300} height={300} className='rounded-md m-auto' />
+          </div>
+        </div>
+        <div className="mt-48 flex flex-row">
+          <div className="info-square-5 rounded-md flex bg-green-400 rounded-md">
+            <Image src='/landing-page-image3.png' alt="background image" width={300} height={300} className='rounded-md m-auto' />
+          </div>
+          <div className="info-square-6 ml-12">
+            <h2 className="text-lg uppercase relative bottom-4 text-gray-400">Feature</h2>
+            <h1 className="font-bold text-lg mb-4 ">Coding is for Everyone</h1>
+            <p className="text-justify mb-8">No matter what is your background, you can also learn how to code. So
+              follow my blog to be up to date with the newest technologies involved in programming</p>
+            <a href="#" className="text-sm text-gray-400 ml-4">Browse articles</a>
+          </div>
+        </div>
+        <div className='mt-48 flex flex-row'>
+          <div className="info-square-7 mt-16">
+            <h2 className="uppercase text-4xl">Connect</h2>
+          </div>
+          <div className="ml-28">
+            <div className="flex flex-row">
+              <div className="info-square-left">
+                <a href="#"><img className="svg-white svg-landing relative bottom-10 left-10" src="github.svg" alt="Github" /></a>
+                <a href="#"><img className="svg-white svg-landing" src="youtube.svg" alt="YouTube" /></a>
+                <a href="#" className="relative top-10 right-10"><img className="svg-white svg-landing" src="discord.svg" alt="Discord" /></a>
+              </div>
+              <div className="info-square-right">
+                <a href="#"><img className="svg-white svg-landing relative bottom-10 left-10" src="instagram.svg" alt="Instagram" /></a>
+                <a href="#"><img className="svg-white svg-landing" src="leetcode.svg" alt="Leetcode" /></a>
+                <a href="#"><img className="svg-white svg-landing relative top-10 right-10" src="linkedin.svg" alt="Linkedin" /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full bg-green-400 flex p-10 flex-col">
+          <h1 className="uppercase text-4xl m-auto">Get Started</h1>
+          <p className="m-auto mt-8">No experience required. Start your coding journey today!</p>
+          <div className="m-auto mt-4 justify-between flex w-60">
+            <button className="rounded-2xl w-28 h-10 bg-gray-800 ">Start Now</button>
+            <button className="rounded-2xl w-28 h-10 bg-gray-800">Contact Me</button>
           </div>
         </div>
       </div>
--- frontend/blog/components/Footer.tsx
+++ frontend/blog/components/Footer.tsx
@@ -1,16 +1,14 @@
 const Footer: React.FC = () => {
   return (
-    <div className='bg-zinc-600 text-white '>
+    <div className='grid text-white font-sans font-roboto grid-cols-4 p-6'>
       <div>
-        <h3>Contact Links</h3>
         <ul>
-          <li>heitorrdpp@gmail.com</li>
-          <li>heitorprado06</li>
-          <li>Heitor Prado</li>
+          <li className="flex flex-row"><img className="svg-white mr-2" src="google.svg" alt="gmail" width={20} height={20} />heitorrdpp@gmail.com</li>
+          <li className="flex flex-row"><img className="svg-white mr-2" src="instagram.svg" alt="instagram" width={20} height={20} />heitorprado06</li>
+          <li className="flex flex-row"><img className="svg-white mr-1" src="linkedin.svg" alt="linkedin" width={25} height={25} />Heitor Prado</li>
         </ul>
       </div>
       <div>
-        <h3>Site Links</h3>
         <ul>
           <li>Blog</li>
           <li>GitHub</li>
@@ -22,6 +20,15 @@ const Footer: React.FC = () => {
           <li>Contact</li>
           <li>FAQ</li>
           <li>Privacy Policy</li>
+        </ul>
+      </div>
+      <div>
+
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
       </div>
     </div>
