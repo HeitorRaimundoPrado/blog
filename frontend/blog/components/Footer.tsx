a579e5f837e33984aeac0223e3c1dda3b71a8b7c
HeitorRaimundoPrado <heitorrdpp@gmail.com>
2023-12-15 22:21:38 -0300

    feat: start working on landing page

---
+++ frontend/blog/components/Footer.tsx
@@ -0,0 +1,31 @@
+const Footer: React.FC = () => {
+  return (
+    <div className='bg-zinc-600 text-white '>
+      <div>
+        <h3>Contact Links</h3>
+        <ul>
+          <li>heitorrdpp@gmail.com</li>
+          <li>heitorprado06</li>
+          <li>Heitor Prado</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Site Links</h3>
+        <ul>
+          <li>Blog</li>
+          <li>GitHub</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>About Me</li>
+          <li>Contact</li>
+          <li>FAQ</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ frontend/blog/components/NavBar.tsx
@@ -0,0 +1,17 @@
+const NavBar: React.FC = () => {
+  return (
+    <>
+      <nav className='bg-zinc-600'>
+        <button>Home</button>
+
+        <ul>
+          <li>About</li>
+          <li>Articles</li>
+          <li>Shop</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar;
